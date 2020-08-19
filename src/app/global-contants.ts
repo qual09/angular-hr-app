a2d66996749c58b0906b65e74a9158e57b714e66
Hieu Chu Pham Xuan <hieu.chu@lingarogroup.com>
2020-08-19 15:07:40 +0200

    company

--- src/app/global-contants.ts
+++ src/app/global-contants.ts
@@ -1,5 +1,5 @@
 export class GlobalConstants {
-    public static companyName: string = 'Lingaro';
+    public static companyName: string = 'Hieu CPX';
     public static currentYear: number = new Date().getFullYear();
-    public static siteTitle: string = 'Lingaro HR Application';
+    public static siteTitle: string = 'HR Application';
 }
