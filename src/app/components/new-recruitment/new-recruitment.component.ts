6b535811221b3bdf69e33f4aaaf6fbcc3c460b2d
Hieu Chu Pham Xuan <hieu.chu@lingarogroup.com>
2020-08-19 16:29:24 +0200

    upd

--- src/app/components/new-recruitment/new-recruitment.component.ts
+++ src/app/components/new-recruitment/new-recruitment.component.ts
@@ -20,7 +20,7 @@ export class NewRecruitmentComponent implements OnInit {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
-    let raw = JSON.stringify({"variables":{"formData":{"value":{"briefingForm":{"openPosition":"Pega Developer","experienceLevel":"","minSalary":100,"maxSalary":150,"contractType":"","location":[],"remoteWork":"","lineManager":"John Wayne","projectOverview":"","projectDuration":"","projectPhase":"","team":"","projectMethodology":"","jobDescription":"","techStack":[],"softSkills":[],"languages":[]},"candidates":[],"hrApproval":false,"status":"New"},"valueInfo":{"objectTypeName":"com.lingaro.firstApp.model.hr.OpenPosition","serializationDataFormat":"application/json"}}}});
+    let raw = JSON.stringify({ "variables": { "formData": { "value": { "briefingForm": { "openPosition": "Pega Developer", "experienceLevel": "", "minSalary": 100, "maxSalary": 150, "contractType": "", "location": [], "remoteWork": "", "lineManager": "John Wayne", "projectOverview": "", "projectDuration": "", "projectPhase": "", "team": "", "projectMethodology": "", "jobDescription": "", "techStack": [], "softSkills": [], "languages": [] }, "candidates": [], "hrApproval": false, "status": "New" }, "valueInfo": { "objectTypeName": "com.company.firstApp.model.hr.OpenPosition", "serializationDataFormat": "application/json" } } } });
 
     let requestOptions: RequestInit = {
       method: 'POST',
@@ -35,7 +35,7 @@ export class NewRecruitmentComponent implements OnInit {
       .catch(error => console.log('error', error));
   }
 
-  alert(){
+  alert() {
     //alert('Create not implemented!');
     console.log('Created!');
   }
